e387ac5f021cf403df167e65dca0cf41fd3d610a
Po Lien Lin <polien.joe.lin@gmail.com>
2023-12-31 00:03:33 +0800

    Update initial state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import DigimonImagesWithLoader from "./DigimonImages";
-import DogImagesWithLoader from "./DogImages";
 import withStyles from "./HOC/withStyles";
-import Pokemon from "./Pokemon";
+// import DigimonImagesWithLoader from "./DigimonImages";
+// import DogImagesWithLoader from "./DogImages";
+// import Pokemon from "./Pokemon";
 
-const LPButton = () => {
-  return <button>師父造我有何用</button>;
+const LPButton = (props) => {
+  return <button {...props}>師父造我有何用</button>;
 };
 
-const LPText = () => {
-  return <p>text</p>;
+const LPText = (props) => {
+  return <p {...props}>{props.text}</p>;
 };
 
 const StyledButton = withStyles(LPButton);
@@ -22,14 +22,14 @@ const App = () => {
       </h1>
       <StyledButton />
       <StyledText />
-      <h1 className="my-3 cursor-pointer font-mono text-2xl font-bold">
+      {/*       <h1 className="my-3 cursor-pointer font-mono text-2xl font-bold">
         咚咚鼠
       </h1>
       <Pokemon />
       <h1 className="mt-8 text-3xl font-bold">狗勾</h1>
       <DogImagesWithLoader />
       <h1 className="mt-8 text-3xl font-bold">亞古獸</h1>
-      <DigimonImagesWithLoader />
+      <DigimonImagesWithLoader /> */}
     </div>
   );
 };
