36d66395bc6fbfcf45ca241ae72662e0f2f590ed
Po Lien Lin <polien.joe.lin@gmail.com>
2023-12-30 17:19:43 +0800

    Refactor props order

--- src/HOC/withStyles.jsx
+++ src/HOC/withStyles.jsx
@@ -2,7 +2,7 @@ const withStyles = (Component) => {
   return (props) => {
     const style = "p-2 m-4 bg-blue-300";
     return (
-      <Component className={style} {...props} test="#22 F2E is the best" />
+      <Component className={style} test="#22 F2E is the best" {...props} />
     );
   };
 };
