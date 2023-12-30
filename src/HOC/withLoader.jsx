48af25d09362107b2ca32282e53b68edbfb1ce7e
Po Lien Lin <polien.joe.lin@gmail.com>
2023-12-31 00:09:42 +0800

    Add comment

--- src/HOC/withLoader.jsx
+++ src/HOC/withLoader.jsx
@@ -3,9 +3,12 @@ import useDataLoader from "../Hook/useDataLoader";
 const withLoader = (Element, url) => {
   const withLoaderHOC = (props) => {
     const data = useDataLoader(url);
+
+    //  以下這個條件句，就是HOC幫助你減少重複寫在每個要fetch api的component的內容
     if (!data) {
       return <span className="loading loading-bars loading-md"></span>;
     }
+    //
 
     return <Element {...props} data={data} />;
   };
--- src/HOC/withStyles.jsx
+++ src/HOC/withStyles.jsx
@@ -1,5 +1,6 @@
 const withStyles = (Component) => {
   return (props) => {
+    //  HOC 幫助你不用在每個 component 都寫一次一樣的style
     const style = {
       padding: "16px",
       margin: "16px",
