2163c8f74a57309bcfe5f6fe5fd746bf1f37c0ff
Po Lien Lin <polien.joe.lin@gmail.com>
2023-12-30 13:58:13 +0800

    Add withLoader HOC

---
+++ src/withLoader.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function withLoader(Element, url) {
+  return (props) => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      async function getData() {
+        const res = await fetch(url);
+        const data = await res.json();
+        setData(data);
+      }
+
+      getData();
+    }, []);
+
+    if (!data) {
+      return <span className="loading loading-bars loading-md"></span>;
+    }
+
+    return <Element {...props} data={data} />;
+  };
+}
